ec553ed008fa48d89f7a4e54d4e3b7fec42a7695
Avel111 <57135174+Avel111@users.noreply.github.com>
2020-08-25 03:54:57 +0300

    test

--- scripts/burger-menu.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.burger')
-const menu = document.querySelector('.header__nav')
-const body = document.querySelector('body')
-
-burger.onclick = event=>{
-    burger.classList.toggle('active')
-    menu.classList.toggle('active')
-    body.classList.toggle('locked')
-}
